8a9b2360ca3a8c57744128795df5c47d1b1fb450
hamdi1611 <hamdiahmadzuhri@gmail.com>
2018-09-28 17:26:19 +0700

    DONE. sorry if there is still any error, but i think, it is good enough to use.

--- help.py
+++ help.py
@@ -1,4 +1,5 @@
 from Number import Number
+from State import State
 
 def isRight(listX):
     more = 0
@@ -28,3 +29,43 @@ def solve(s):
                         temp.append(a)
             container = temp
         return container
+
+def component(a, problem):
+    for li in problem.getList():
+        for e in li:
+            if a == e.getAlpha():
+                return e
+    return Number(a)
+
+def inputProblem():
+    problem = State()
+    print ("How many words do you want type into addition? (not include a word as a solution)")
+    n = int(input("Input positive integer: "))
+    temp = ""
+    for i in range(n+1):
+        word = input()
+        temp += word
+        problem.getList().append([])
+        if i==n-1:
+            for j in range(0, len(problem.getList()[0])):
+                print('-', end="")
+            print('--- +')
+        for a in word:
+            problem.getList()[i].insert(0, component(a, problem))
+    if len(set(temp)) > 10:
+        return State()
+    else:
+        return problem
+
+def printSolution(s):
+    n = len(s.getList())
+    for i in range(0, n):
+        if i == n-1:
+            for j in range(0, len(s.getList()[i])):
+                print('-', end="")
+            print('--- +')
+        temp = []
+        for e in s.getList()[i]:
+            temp.insert(0, str(e.getValue()))
+        print(''.join(temp))
+    
--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-from Number import Number
-from State import State
 import help
 
-# Example : SEND + MORE = MONEY
+# Example : input integer 2, then SEND + MORE = MONEY
+problem = help.inputProblem()
 
-z = Number('S')
-e = Number('E')
-n = Number('N')
-d = Number('D')
-m = Number('M')
-o = Number('O')
-r = Number('R')
-y = Number('Y')
-
-s = State()
-s.getList().append([d, n, e, z])
-s.getList().append([e, r, o, m])
-s.getList().append([y, e, n, o, m])
-
-print ('Solusi:')
-answer = help.solve(s)
-print ([[(x.getAlpha(), x.getValue()) for x in apa] for apa in answer[0].getList()])
+if len(problem.getList())==0:
+    print('\nThere in no answer')
+else:
+    answer = help.solve(problem)
+    if len(answer)==0:
+        print('\nThere in no answer')
+    else:
+        print ('\nSolution:')
+        for s in answer:
+            help.printSolution(s)
+            print('')
